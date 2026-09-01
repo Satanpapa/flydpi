6cc60331544c040a12c5ccc2f47d2ed258c12b3d
maxiQ <166013947+Satanpapa@users.noreply.github.com>
2026-09-02 03:06:13 +1000

    feat: expose diagnostic report to QML

--- ui/qt/RpcClient.h
+++ ui/qt/RpcClient.h
@@ -2,23 +2,29 @@
 
 #include <QObject>
 #include <QTcpSocket>
+#include <QVariantMap>
 #include <QJsonObject>
 
 class RpcClient final : public QObject {
     Q_OBJECT
     Q_PROPERTY(bool connected READ connected NOTIFY connectedChanged)
+    Q_PROPERTY(QVariantMap diagnosticReport READ diagnosticReport NOTIFY diagnosticReportChanged)
+
 public:
     explicit RpcClient(QObject* parent = nullptr);
+
     bool connected() const;
+    QVariantMap diagnosticReport() const;
 
     Q_INVOKABLE void connectToOrchestrator();
     Q_INVOKABLE void statusGet();
-    Q_INVOKABLE void probeRun(const QStringList& targets);
+    Q_INVOKABLE void probeRun(const QStringList& targets = {});
 
 signals:
     void connectedChanged();
-    void resultReceived(quint64 id, const QString& json);
+    void resultReceived(quint64 id, const QString& method, const QString& json);
     void errorOccurred(const QString& message);
+    void diagnosticReportChanged();
 
 private slots:
     void onConnected();
@@ -27,7 +33,10 @@ private slots:
 
 private:
     void send(const QString& method, const QJsonObject& params);
+    void handleResult(quint64 id, const QJsonObject& result);
+
     QTcpSocket m_socket;
     quint64 m_nextId{1};
     QByteArray m_buffer;
+    QVariantMap m_diagnosticReport;
 };
