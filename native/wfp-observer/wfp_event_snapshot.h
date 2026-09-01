ca208111478a0888ff667ca3df00f42b463f36f6
maxiQ <166013947+Satanpapa@users.noreply.github.com>
2026-09-02 02:54:10 +1000

    fix: align WFP snapshot with Windows event header

--- native/wfp-observer/wfp_event_snapshot.h
+++ native/wfp-observer/wfp_event_snapshot.h
@@ -6,23 +6,15 @@
 extern "C" {
 #endif
 
-typedef enum FlyDpiEventKind {
-    FLYDPI_EVENT_OTHER = 0,
-    FLYDPI_EVENT_CLASSIFY_DROP = 1,
-    FLYDPI_EVENT_AUTH_CONNECT = 2,
-    FLYDPI_EVENT_AUTH_RECV_ACCEPT = 3,
-} FlyDpiEventKind;
-
 typedef struct FlyDpiEventSnapshot {
     uint64_t timestamp_100ns;
+    uint32_t flags;
     uint32_t ip_version;
     uint8_t protocol;
-    uint8_t direction;
     uint16_t local_port;
     uint16_t remote_port;
-    uint32_t process_id;
-    uint32_t event_code;
-    uint32_t status;
+    uint32_t event_type;
+    uint32_t result_code;
 } FlyDpiEventSnapshot;
 
 #ifdef __cplusplus
